301a7e34e8be8439073047d0c3112f8fd3e33cde
Daniel Windsor <dw.accounts@protonmail.com>
2020-05-07 09:23:06 +1200

    Add user reducer

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,8 +3,13 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 
 import LandingPage from './LandingPage'
 
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:4000')
+
 class App extends React.Component {
   render() {
+    console.log(socket.id)
     return (
       <Router>
         <Route path='/' component={LandingPage} />
---
+++ client/reducers/users.js
@@ -0,0 +1,11 @@
+const users = (state = [], action) => {
+  switch(action.type) {
+    case 'SET_USER' :
+      return state
+
+    default:
+      return state
+  }
+}
+
+export default users
