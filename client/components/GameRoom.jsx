b1f035bd16645db78a1df35a566bc7846cc836d6
tom-rose-codeman <tomrlrose@gmail.com>
2020-05-07 14:25:30 +1200

    make initial gameroom layout without timer

---
+++ client/components/GameRoom.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import { connect } from 'react-redux'
+import { render } from 'enzyme'
+
+class GameRoom extends React.Component {
+
+    handleVote = e => {
+        this.props.dispatch(doVote())
+    }
+
+    handleSkip = e => {
+        this.props.dispatch(doSkip())
+    }
+
+    handleComplete = e => {
+        this.props.dispatch(doComplete())
+    }
+
+
+
+
+
+    render(){
+
+        let display;
+        if (this.props.localUser.role == 'alien') {
+            display =   <div>
+                            <p>{this.props.localUser.task}</p>
+                            <button onCick={this.handleSkip}>skip</button>
+                            <button onClick={this.handleComplete}>complete</button>
+                        </div>
+        } else {
+            display = <div>
+                        <p>{this.props.localUser.hint}</p>
+                        </div>
+        }
+
+
+
+        return(
+            <div>
+                <h1>you are a ${this.props.localUser.role}</h1>
+
+                {display}
+              
+                <button onClick={this.handleVote}>Vote!</button>
+
+            </div>
+        )
+    }
+    
+    
+}
+
+function mapStateToProps(globalState) {
+    return {
+      socket: globalState.localUser.socket,
+      user: globalState.localUser,
+      hint: globalState.hint
+    }
+  }
+
+
+
+export default connect(mapStateToProps)(GameRoom)
