e9fc2d20a0b3342a0ba5c4e4ba013b3404224a61
Hieu Tran <tranhieunz@gmail.com>
2020-05-10 12:16:31 +1200

    Default set

--- client/components/Voting.jsx
+++ client/components/Voting.jsx
@@ -63,13 +63,13 @@ class Voting extends React.Component {
           <div>
             {this.props.voter === this.props.localUser.name ?
               <>
-                <h1>You accused <strong>{this.props.vote}</strong> of being an <strong>Alien!</strong></h1>
+                <h1>You accused <strong>{this.props.vote}</strong> of being an <strong>{'\u{1F47D}'}</strong></h1>
                 <p>Who will agree with you?</p>
               </>
               :
               <>
-                <h1><strong>{this.props.voter}</strong> thinks <strong>{this.props.vote}</strong> is an <strong>Alien!</strong> How shall we proceed?</h1>
-                <h3>{this.state.vote}</h3>
+                <h1><strong>{this.props.voter}</strong> thinks <strong>{this.props.vote}</strong> is an <strong>{'\u{1F47D}'}</strong> How shall we proceed?</h1>
+                {/* <h3>{this.state.vote}</h3> */}
                 <button onClick={() => this.sendVote(true)}>Alien Autopsy!</button>
                 <button onClick={() => this.sendVote(false)}>{this.props.vote} is a human!</button>
               </>
