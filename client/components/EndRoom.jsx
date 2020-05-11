97906b34b58f5300f3de7517843fd9e444f8f406
Simon Hall <simon970@gmail.com>
2020-05-11 15:16:41 +1200

    update classnames

--- client/components/EndRoom.jsx
+++ client/components/EndRoom.jsx
@@ -15,32 +15,32 @@ class EndRoom extends React.Component {
     return (
       <div className='align' >
         <section className = "light">
-          <h3 className='heading gradient1'>The winner is...</h3>
+          <h3 className='header3'>The winner is...</h3>
           {/*<h1>{this.props.location.state.winner}!</h1>*/}
-          <h1 className='gradient2'>Alien!</h1>
+          <h1 className='header'>Alien!</h1>
 
         {/*<div><h3>Final Time: {this.props.location.state.time}</h3></div>*/}
         </section>
         
         <div><h3>Final Time: 1:15</h3></div>
 
-        <div>
+        <div className='display'>
           <h2 className='text'>The alien completed these tasks-</h2>
-          <div className='tasklist'>
-            <p className='completedTask'>This is a task that the Alien completed - complete</p>
-            <p className='skippedTask'>The Alien decided to skip this task - skipped</p>
-            <p className='completedTask'>This is a task that the Alien completed - complete</p>
-            <p className='skippedTask'>The Alien decided to skip this task - skipped</p>
-            <p className='completedTask'>This is a task that the Alien completed - complete</p>
-            <p className='skippedTask'>The Alien decided to skip this task - skipped</p>
-            <p className='completedTask'>This is a task that the Alien completed - complete</p>
-            <p className='skippedTask'>The Alien decided to skip this task - skipped</p>
-            <p className='completedTask'>This is a task that the Alien completed - complete</p>
-            <p className='skippedTask'>The Alien decided to skip this task - skipped</p>
-            <p className='completedTask'>This is a task that the Alien completed - complete</p>
-            <p className='skippedTask'>The Alien decided to skip this task - skipped</p>
-            <p className='completedTask'>This is a task that the Alien completed - complete</p>
-            <p className='skippedTask'>The Alien decided to skip this task - skipped</p>
+          <div>
+            <p className='task completedTask'>This is a task that the Alien completed - complete</p>
+            <p className='task skippedTask'>The Alien decided to skip this task - skipped</p>
+            <p className='task completedTask'>This is a task that the Alien completed - complete</p>
+            <p className='task skippedTask'>The Alien decided to skip this task - skipped</p>
+            <p className='task completedTask'>This is a task that the Alien completed - complete</p>
+            <p className='task skippedTask'>The Alien decided to skip this task - skipped</p>
+            <p className='task completedTask'>This is a task that the Alien completed - complete</p>
+            <p className='task skippedTask'>The Alien decided to skip this task - skipped</p>
+            <p className='task completedTask'>This is a task that the Alien completed - complete</p>
+            <p className='task skippedTask'>The Alien decided to skip this task - skipped</p>
+            <p className='task completedTask'>This is a task that the Alien completed - complete</p>
+            <p className='task skippedTask'>The Alien decided to skip this task - skipped</p>
+            <p className='task completedTask'>This is a task that the Alien completed - complete</p>
+            <p className='task skippedTask'>The Alien decided to skip this task - skipped</p>
           </div>
           {/* commented out for styling
           {
@@ -64,7 +64,7 @@ class EndRoom extends React.Component {
         <div>
           <p className='text'>this.state.time goes here</p>
         </div >
-        <button className='button' onClick={() => this.props.history.push('/waiting')
+        <button className='vote-btn' onClick={() => this.props.history.push('/waiting')
         }> Another game ?</button >
       </div >
     )
