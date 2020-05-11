a6fed20e9aa6c488d63793698828838dd23d11cf
Simon Hall <simon970@gmail.com>
2020-05-11 12:10:45 +1200

    hard code for styling end page

--- client/components/EndRoom.jsx
+++ client/components/EndRoom.jsx
@@ -11,17 +11,26 @@ class EndRoom extends React.Component {
   }
 
   render() {
+    let tasklist = () => {
+      
+    }
     return (
       <div className='align' >
         <div>
           <h3 className='heading'>The winner is...</h3>
-          <h1>{this.props.location.state.winner}!</h1>
+          {/*<h1>{this.props.location.state.winner}!</h1>*/}
+          <h1>Alien!</h1>
         </div >
 
-        <div><h3>Final Time: {this.props.location.state.time}</h3></div>
+        {/*<div><h3>Final Time: {this.props.location.state.time}</h3></div>*/}
+        <div><h3>Final Time: 1:15</h3></div>
+
 
         <div>
           <h2 className='text'>The alien completed these tasks-</h2>
+          <p style={color='green'}>{task.task} - complete</p>
+          <p style={color='red'}>{task.task} - skipped</p>
+          {/* commented out for styling
           {
             this.props.location.state.taskList.map(task => {
               if (task.complete == true) {
@@ -37,6 +46,7 @@ class EndRoom extends React.Component {
               }
             })
           }
+        */}
         </div >
 
         <div>
