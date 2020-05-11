0fcca2fb2cd9f6ddc7cc5b837ecb700a540dc158
Simon Hall <simon970@gmail.com>
2020-05-12 02:47:36 +1200

    walking space pants

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -28,15 +28,20 @@ class LandingPage extends React.Component {
   render() {
     return (
       <div className='wrapper'>
-        <img className = 'lp-background' src='./img/WC-pink-transparent2.png'/>
-        <div className="container">
+        <img className = 'pants'/>
+        <img className = 'lightblue' src='./img/bg-lightblue-space.png'/>
+        <img className = 'darkblue' src='./img/bg-darkblue-space.png'/>
+        <img className = 'purple' src ='./img/bg-purple-space.png'/>
+        
+        {/* <div className = 'pants step2'/>
+        
+        {/* <div className="container">
           <h1 className='heading'>SpacePants</h1>
           <button className='button' onClick={() => this.props.history.push('/create')}>Create Game</button>
           <button className='button' onClick={() => this.props.history.push('/join')}>Join Game</button>
           <button className="button" id="centered-toggle-button" onClick={this.showModal}>How to play</button>
-
           <HowToModal showModal={this.state.showModal} closeModal={this.closeModal} />
-        </div>
+        </div> */}
         
                  
       </div>
