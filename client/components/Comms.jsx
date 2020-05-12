c7f9597c7bc4a0ae115b6acd7d6e766931ff4940
Daniel Windsor <dw.accounts@protonmail.com>
2020-05-12 16:53:22 +1200

    Adjust time

--- client/components/Comms.jsx
+++ client/components/Comms.jsx
@@ -52,7 +52,7 @@ class Comms extends React.Component {
   handleComplete = e => {
     let [minutes, seconds] = this.props.time.split(':')
 
-    if (Number(seconds) < 35 && minutes == '0') {
+    if (Number(seconds) < 30 && minutes == '0') {
       this.setState({
         task: 'No more submissions needed!  You have successfully integrated into human culture.  Surely they won\'t discover you in the final few seconds...'
       })
